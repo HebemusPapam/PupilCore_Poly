75c22814301a514a2baae19b89c61a0d91c6bdb0
Elliot Bouchet <Elliot.Bouchet@etu.univ-grenoble-alpes.fr>
2023-02-27 14:28:01 +0100

    Fonctionnement avec choix méthode

---
+++ Gaze_recording/Visualization/Dispersion_map_visualization.py
@@ -0,0 +1,314 @@
+################## Import package ##################
+from tkinter import ttk
+import tkinter as tk
+import math as mt
+import warnings
+import os
+import numpy as np
+import h5py
+import platform
+import matplotlib.pyplot as plt
+from scipy.ndimage import gaussian_filter
+from matplotlib.colors import ListedColormap
+import pandas
+import Methode_I_DT as IDT
+
+
+################## Parameters ##################
+# experiment data directory and files
+PATH = os.getcwd()
+print(PATH)
+if platform.system() == 'Windows' :
+    IMG_PATH = PATH + '\Gaze_recording\ExplorationImgCoder\img\\'
+    #IMG_PATH = 'D:\Cours\IESE4\PupilCore_POLYTECH\PupilCore_Poly\Gaze_recording\ExplorationImgCoder\img\\'
+    HDF_PATH = PATH + '\Gaze_recording\ExplorationImgCoder\data\\'
+    #HDF_PATH = 'D:\Cours\IESE4\PupilCore_POLYTECH\PupilCore_Poly\Gaze_recording\ExplorationImgCoder\data\\'
+elif platform.system() == 'Darwin':
+    IMG_PATH = PATH + '/Gaze_recording/ExplorationImgCoder/img/'
+    HDF_PATH = PATH + '/Gaze_recording/ExplorationImgCoder/data/'
+
+
+#FILENAME =  ['ExploIMG_PupilCore_m_001.hdf5','ExploIMG_Tobii_d_001.hdf5'] # files to visualize
+FILENAME =  ['ExploIMG_PupilCore_m_001.hdf5']
+
+# parameters useful just to init gaze data array's size which must be above T_IMG*F_TRACKER_MAX
+T_IMG = 7           # duration of the image exploration phase in seconds
+F_TRACKER_MAX = 120 # highest sampling frequency in Hz of the eyetrackers used during FILENAME experiments
+
+# Defining the dataframe which will contain all the informations about the fixations
+FIXATION_INFORMATION = pandas.DataFrame()
+SACCADE_INFORMATION = pandas.DataFrame()
+
+################## Function definitions ##################
+def validate():
+    """
+    Get the user choice made in the widget's menu
+    """
+    global DATA_CHOICE
+    global PLOT_CHOICE
+    global RADIUS_CHOICE
+    global DURATION_CHOICE
+    DURATION_CHOICE = 0.001 * float(w3.get())
+    RADIUS_CHOICE = float(w2.get())
+    DATA_CHOICE = w1.get()
+    PLOT_CHOICE = w.get()
+    win.destroy()
+
+def dispersion_plot(image_name,cercle,extent,image,win_size,radius):
+    fig, ax = plt.subplots()
+    ax.set_title('Raw gaze plot : '+image_name)
+    for center in cercle :
+        print("centre ",center)
+        ax[0].add_artist(plt.Circle((center[0],center[1]),center[2],linewidth = 2, fill=0 ))#,color = 'red'
+
+    ax[0].imshow(image, extent=extent)    
+    ax[0].set_xlim([-int(win_size[0])/2,int(win_size[0])/2])
+    ax[0].set_ylim([-int(win_size[1])/2,int(win_size[1])/2])
+
+    #Pour la 2eme image : 
+    ax[1].set_title('Dispersion gaze plot : '+image_name)
+    ax[1].plot(time,x,label='x')
+    ax[1].plot(time,y,label='y')
+    #plt.plot(time,x,label='x')
+    #plt.plot(time,y,label='y')
+   
+    #Plot des deux images dans une 
+    ax[1].axis('on')
+    ax[1].legend()
+    ax[0].axis('on')
+    ax[0].legend()
+    plt.show()
+
+def raw_gaze_plot(time,image_name,x,y,filename,extent,image,win_size):
+    """Fonction qui affiche les points de regard sur l'image"""
+    fig, ax = plt.subplots(2,sharex=False, sharey=False)
+
+    #Pour la 1ere image : 
+    ax[0].set_title('Raw gaze plot : '+image_name)
+    ax[0].plot(x, y, marker='',   # marker='.'
+            linewidth=1, markersize=6,
+            linestyle='-',fillstyle='full', #linestyle='dashed'
+            label=filename)
+        
+    ax[0].imshow(image, extent=extent)    
+    ax[0].set_xlim([-int(win_size[0])/2,int(win_size[0])/2])
+    ax[0].set_ylim([-int(win_size[1])/2,int(win_size[1])/2])
+
+    #Pour la 2eme image : 
+    ax[1].set_title('Raw gaze plot : '+image_name)
+    ax[1].plot(time,x,label='x')
+    ax[1].plot(time,y,label='y')
+    #plt.plot(time,x,label='x')
+    #plt.plot(time,y,label='y')
+   
+    #Plot des deux images dans une 
+    ax[1].axis('on')
+    ax[1].legend()
+    ax[0].axis('on')
+    ax[0].legend()
+    plt.show()
+
+def Disper_raw_plot(time,image_name,x,y,filename,extent,image,win_size,cercle):
+    """Fonction qui affiche les points de regard sur l'image"""
+    fig, ax = plt.subplots(2,sharex=False, sharey=False)
+    ax[0].set_title('Dispersion & Raw gaze plot : '+image_name)
+    ax[0].plot(x, y, marker='',   # marker='.'
+            linewidth=1, markersize=6,
+            linestyle='-',fillstyle='full', #linestyle='dashed'
+            label=filename)
+    for center in cercle :
+        ax[0].add_artist(plt.Circle((center[0],center[1]),center[2],
+                                linewidth = 2, fill=0 ,color = 'blue'))
+    ax[0].imshow(image, extent=extent)    
+    ax[0].set_xlim([-int(win_size[0])/2,int(win_size[0])/2])
+    ax[0].set_ylim([-int(win_size[1])/2,int(win_size[1])/2])
+
+    #Pour la 2eme image : 
+    ax[1].set_title('Dispersion & Raw gaze plot : '+image_name)
+    ax[1].plot(time,x,label='x')
+    ax[1].plot(time,y,label='y')
+    #plt.plot(time,x,label='x')
+    #plt.plot(time,y,label='y')
+   
+    #Plot des deux images dans une 
+    ax[1].axis('on')
+    ax[1].legend()
+    ax[0].axis('on')
+    ax[0].legend()
+    plt.show()
+    
+
+def Save_fixation(cercle,participant,image):
+    #on ajoute le nom et numéro du participant de l'éxperience 
+    participant = participant.split('_')
+    nom = participant[2] + ' n° ' + participant[3][0:len(participant[3])-5]
+    #on transforme le tableau en dataframe afin d'être sauvegardé
+    Cercle_array = np.array(cercle,dtype=[('Fixation_x','<i1'),('Fixation_y','<i1'),('rayon','<f2'),('Time Start','<f2'),('Duration','<f2')])
+    FIXATION_INFORMATION = pandas.DataFrame(Cercle_array, columns=['Fixation_x','Fixation_y','rayon','Time Start','Duration'])
+    FIXATION_INFORMATION.insert(0, 'INFORMATIONS', pandas.Series([nom,image], index=[0,1]))
+    print(FIXATION_INFORMATION)
+
+def Save_Saccade(Saccade,participant,image):
+    #on ajoute le nom et numéro du participant de l'éxperience 
+    participant = participant.split('_')
+    nom = participant[2] + ' n° ' + participant[3][0:len(participant[3])-5]
+    #on transforme le tableau en dataframe afin d'être sauvegardé
+    Saccade_array = np.array(Saccade,dtype=[('Type',np.unicode_, 16), ('X_start','<i1'), ('Y_start','<i1'), ('X_end','<i1'), ('Y_end','<i1'),('Time Start','<f2'),('Time End','<f2'),('Duration','<f2')])
+    SACCADE_INFORMATION = pandas.DataFrame(Saccade_array, columns=['Type','X_start','Y_start','X_end','Y_end','Time Start','Time End','Duration'])
+    SACCADE_INFORMATION.insert(0, 'INFORMATIONS', pandas.Series([nom,image], index=[0,1]))
+    print(SACCADE_INFORMATION)
+
+
+def find_first_index(lst, condition):
+    return [i for i, elem in enumerate(lst) if condition(elem)][0]
+################## Search image files used in /IMG_PATH ##################
+img_list = []
+
+for f in os.listdir(IMG_PATH):
+    ext = os.path.splitext(f)[1]
+    if ext.lower() not in [".jpg",".png"]:
+        continue
+    img_list.append(f)
+
+nb_image = np.size(img_list)
+
+################## Dialog box to set visualization choice ##################
+# config the widget window
+win = tk.Tk()  # init the widget
+win.geometry('500x140')
+win.title('Plot parameters')
+
+# config the duration thresold
+w3 = ttk.Label(win, text = "Minimum fixation duration (ms) :")
+w3.grid(column = 0,row = 4, padx = 10, pady = 5)
+w3 = ttk.Entry(win)
+w3.insert(0,"150") #Valeur du temps
+w3.grid(row=4,column=1,padx=10,pady=5)  # adding to grid
+
+# config the dispersion radius
+w2 = ttk.Label(win, text = "Maximum fixation dispersion:")
+w2.grid(column = 0,row = 3, padx = 10, pady = 5)
+w2 = ttk.Entry(win)
+w2.insert(0,"150") #Valeur du rayon
+w2.grid(row=3,column=1,padx=10,pady=5)  # adding to grid
+
+# config the box menu data choice
+w1 = ttk.Label(win, text = "Select binocular data :")
+w1.grid(column = 0,row = 1, padx = 10, pady = 5)
+w1 = ttk.Combobox(win, values = ['Left eye', 'Right eye', 'Average both eyes']) #box menu
+w1.grid(row=1,column=1,padx=10,pady=5)  # adding to grid
+w1.set('Average both eyes')             # default selected option
+
+# config the box menu plot choice
+w = ttk.Label(win, text = "Select plot type :")
+w.grid(column = 0,row = 2, padx = 10, pady = 5)
+w = ttk.Combobox(win, values = ['Dispersion_map', 'Raw_gaze_plot', 'Both']) #box menu
+w.grid(row=2,column=1,padx=10,pady=5)    # adding to grid
+w.set('Both')                   # default selected option
+
+#config the validation button
+b1=tk.Button(win,text="Submit", command=lambda: validate())
+b1.grid(row=3,column=3)
+win.mainloop()
+
+################## Search for each image the corresponding gaze data in all hdf file loaded  ##################
+nb_file  = np.size(FILENAME)
+FRAMES   = T_IMG*F_TRACKER_MAX*3
+
+# --- LOOP OVER FILES --- #
+for s in range(nb_file):
+
+    # --- Import hdf file data --- #
+    # Open hierachical element contained in the HDF data file
+    f = h5py.File(HDF_PATH+FILENAME[s],'r')
+ 
+    # Init 2d arrays to store gaze data for raw gaze plot (size=nb_file,nb_sample_per_trials)
+    gaze_x_raw = np.empty((FRAMES,nb_file))*np.nan
+    gaze_y_raw = np.empty((FRAMES,nb_file))*np.nan
+    gaze_time_xy = np.empty((FRAMES,nb_file))*np.nan
+    # Import Experiment gaze data & events
+    bino_data = f['data_collection']['events']['eyetracker']['BinocularEyeSampleEvent']  # Access to the Binocular eye Samples of the HDF file
+    events    = f['data_collection']['events']['experiment']['MessageEvent']  # access to the binocular gaze data of the HDF file
+
+    
+    for i in range(nb_image): # LOOP OVER IMAGES
+            # ---  load the image to plot --- #
+        img  = plt.imread(IMG_PATH+img_list[i])
+        img_size = img.shape[0:2] # height, width of the loaded image
+        extent = [-img_size[1]/2,img_size[1]/2,-img_size[0]/2,img_size[0]/2] #set the image coordinate origin to the image center
+
+        # --- Research the event id of the first and last samples recorded during the image exploration phase
+        # Research the array index of the image event label
+        index_img_labbel = np.where(events['text'].astype('U128') == img_list[i])
+
+        if len(index_img_labbel[0]) != 0 :  # if the data exist for this image ('img_list[i]' exists)                              
+            
+            # read the time at which the image exploration pahse started
+            index_img_labbel = index_img_labbel[0][0]
+            t_start_img   = events['text'][index_img_labbel-1].astype('U128').replace('IMAGE_START :','')
+
+            if len(events['event_id'])-1 > index_img_labbel+1: # & if the image exploration has been complete (IMG_STOP exists)
+
+                # read the time at which the image exploration phase stoped
+                t_stop_img     = events['text'][index_img_labbel+1].astype('U128').replace('IMAGE_STOP :','')
+
+                # research the index array corresponding in bino_array to the begining and the end of the image exploration
+                ind_gaze_stop  = find_first_index(bino_data['time'], lambda e:e> float(t_stop_img))
+                ind_gaze_start = find_first_index(bino_data['time'], lambda e:e> float(t_start_img))
+                
+                # --- Get gaze data for this image exploration according to the user choice of visualization --- #
+                if DATA_CHOICE == 'Right eye':
+                    img_gaze = np.array([bino_data['right_gaze_x'][ind_gaze_start:ind_gaze_stop], bino_data['right_gaze_y'][ind_gaze_start:ind_gaze_stop]])
+
+                elif DATA_CHOICE == 'Left eye':
+                    img_gaze = np.array([bino_data['left_gaze_x'][ind_gaze_start:ind_gaze_stop], bino_data['left_gaze_y'][ind_gaze_start:ind_gaze_stop]])
+                
+                elif DATA_CHOICE == 'Average both eyes' :  # compute gaze average between both eyes
+                    with warnings.catch_warnings():        # removee RuntimeWarnings from nanmean
+                        warnings.simplefilter("ignore", category=RuntimeWarning)
+                        avg_gaze_x = np.nanmean(np.array([bino_data['left_gaze_x'][ind_gaze_start:ind_gaze_stop],bino_data['right_gaze_x'][ind_gaze_start:ind_gaze_stop]]), axis=0)
+                        avg_gaze_y = np.nanmean(np.array([bino_data['left_gaze_y'][ind_gaze_start:ind_gaze_stop],bino_data['right_gaze_y'][ind_gaze_start:ind_gaze_stop]]), axis=0)
+                        gaze_time = np.array(bino_data['time'][ind_gaze_start:ind_gaze_stop])
+                        img_gaze   = np.array([avg_gaze_x, avg_gaze_y,gaze_time])
+                
+                #get all the value of time for each gaze and each image
+                
+                img_gaze = img_gaze[~np.isnan(img_gaze).any(axis=1)]
+            
+       
+
+        ################## Get the window size used during experiment ##################
+        win_size = events['text'][0].astype('U128').replace('ScreenSize=','')
+        win_size = win_size.replace('[','')
+        win_size = win_size.replace(']','')
+        win_size = list(win_size.split(", "))
+
+        ################## Dispersion map plot #######################
+        if len(img_gaze[0]) != 0 and PLOT_CHOICE == 'Dispersion_map': # if data exist
+            # --- Display the histogram overlap on the reference image --- #
+            Fixation,Saccade = IDT.Choix_Methode_Dispersion("Salvucci",img_gaze[2],img_gaze[0],img_gaze[1],RADIUS_CHOICE,DURATION_CHOICE,FILENAME[s],img_list[i])
+            #def Save_fixation(Fixation,FILENAME[s],img_list[i])
+            Save_Saccade(Saccade,FILENAME[s],img_list[i])
+            dispersion_map(time,gaze_x, gaze_y,radius,duration,participant, image
+            dispersion_plot(img_gaze[2],img_list[i],img_gaze[0],img_gaze[1],Fixation,extent,img,win_size)
+            
+
+        ################## Raw gaze data plot ##################
+        if len(img_gaze[0]) != 0 and PLOT_CHOICE == 'Raw_gaze_plot': # if data exist
+            # --- Display raw gaze data overlap on the reference image --- #
+            raw_gaze_plot(img_gaze[2],img_list[i],img_gaze[0],img_gaze[1],FILENAME,extent,img,win_size)
+           
+        
+        ################## Dispersion map & raw data subplot #######################
+        if len(img_gaze[0]) != 0 and PLOT_CHOICE == 'Both':
+            # --- Display raw gaze + heatmap overlap on the reference image --- #
+            Fixation,Saccade = IDT.Choix_Methode_Dispersion("Salvucci",img_gaze[2],img_gaze[0],img_gaze[1],RADIUS_CHOICE,DURATION_CHOICE)
+            Save_fixation(Fixation,FILENAME[s],img_list[i])
+            Save_Saccade(Saccade,FILENAME[s],img_list[i]) 
+            Disper_raw_plot(img_gaze[2],img_list[i],img_gaze[0],img_gaze[1],FILENAME[s],extent,img,win_size,Fixation) 
+         
+        
+            
+FIXATION_INFORMATION.to_excel('sample_data.xlsx', sheet_name='sheet1', index=True)
+        
+    
---
+++ Gaze_recording/Visualization/Methode_I_DT.py
@@ -0,0 +1,219 @@
+import numpy as np
+import math as mt
+
+def Choix_Methode_Dispersion(methode,time,gaze_x,gaze_y,radius,duration):
+    #dispersion_map_Salvador(time,gaze_x,gaze_y,radius,duration)
+    dispersion_map_Eucl(time,gaze_x,gaze_y,30,duration)
+    if methode=="Salvucci" :
+        return dispersion_map_Salvucci(time,gaze_x,gaze_y,radius,duration)
+    elif methode=="Eucl":
+        return dispersion_map_Eucl(time,gaze_x,gaze_y,radius,duration)
+    
+
+def calcul_disp(Xs,Ys):
+    dispersion = max((Xs[:])) - min((Xs[:])) + max((Ys[:])) - min ((Ys[:]))
+    return dispersion
+
+def Check_Fixation_Salvucci(time,start,end,duration_limit,rayon_dispersion,All_Points_Not_Searched,x,y,radius):
+    #On cherche la dispersion sur une fenêtre de point, consistant au seuil de durée minimum d'une fixation
+    while dispersion < rayon_dispersion and All_Points_Not_Searched:
+        #########Condition Temporel############
+        while((time[end]-time[start])<duration_limit and All_Points_Not_Searched ):
+            if end<(len(x)-2):
+                end+=1
+            else:
+                All_Points_Not_Searched = False
+        ##########Tant que la dispersion est acceptable############
+        #la condition temporel est respecté mais on continu d'ajouté des points tant que dispersion < rayon_dispersion
+        #la dispersion est inférieur au seuil on recommence avec une fenêtre plus grande
+        if end<(len(x)-2):
+            end+=1
+        else:
+            All_Points_Not_Searched = False
+
+        dispersion = calcul_disp(x[start:end],y[start:end])
+
+        #On augment le seuil de dispersion si il y a de plus en plus de points
+        if (All_Points_Not_Searched and dispersion < rayon_dispersion and rayon_dispersion < radius*2):
+            rayon_dispersion = rayon_dispersion *1.001
+    
+    return rayon_dispersion,All_Points_Not_Searched,dispersion,start,end
+
+
+def Check_Micro_Saccade_Salvucci(MicroS,t0,t1,x,y,microS_info,time):
+    MicroS_duration = 0.025 #Durée max d'une Micro saccade : 25ms
+    All_Points_Not_Searched = True
+    #On met de côté les points contenus dans la fixation
+    x1 = x[t0:t1]
+    y1 = y[t0:t1]
+
+    #On n'utilise que l'index au début de la possible micro saccade 
+    t0 = t1
+
+    while(t1-t0<MicroS_duration and All_Points_Not_Searched and not MicroS):
+        if t1<(len(x)-2):
+                t1+=1
+        else :
+            All_Points_Not_Searched = False
+        x2 = np.copy(x1)
+        y2 = np.copy(y1)
+        x2 = np.append(x2,x[t1])
+        y2 = np.append(y2,x[t1])
+        #On vérifie si la dispersion redevient sensiblement identique (l'utilisateur est revenu au point de départ)
+        if abs(calcul_disp(x1,y1)-calcul_disp(x2,y2)) < 30:
+            MicroS = True
+            microS_info.append(("Micro",x[t0],y[t0],x[t1],y[t1],time[t0],time[t1],time[t0]-time[t1]))
+            #On retire les points de la Micro saccade
+            print("Disp : ",calcul_disp(x1,y1), "Disp 2 :", calcul_disp(x2,y2))
+            print(" slice :  ",slice(t0,t1,1),"\n\r")
+            print(" x:  ",x, "\n\r")
+            np.delete(x,slice(t0,t1,1),axis=0)
+            np.delete(y,slice(t0,t1,1),axis=0)
+    return MicroS,x,y,All_Points_Not_Searched,microS_info
+
+def Check_Micro_Saccade_Eucl():
+    
+    return
+
+def dispersion_map_Salvucci(time,gaze_x,gaze_y,radius,duration):
+    """
+    Return a list of fixation : fixation (x,y,radius) if the conditions are respected
+    A circle is created, if the dispersion and time between the points are contained in their respective threshold.
+    Using method of Salvucci, D.D.; Goldberg, J.H. Identifying fixations and saccades in eye-tracking protocols.
+    """
+    rayon_dispersion = radius
+    duration_limit = duration
+    end=0
+    start = 0
+    dispersion = 0
+    fixation = []
+    saccade = []
+
+    #Flag pour les différents évenements possible
+    No_Saccade=True
+    All_Points_Not_Searched = True
+    Micro_Saccade = False
+
+    while (All_Points_Not_Searched):
+        if end<(len(gaze_x)-2):
+                end+=1
+        else :
+            All_Points_Not_Searched = False        
+        #On prend une fenêtre de point sur la durée mini d'une fixation et on vérifie sa dispersion
+        rayon_dispersion,All_Points_Not_Searched,dispersion,start,end = Check_Fixation_Salvucci(time,start,end,duration_limit,rayon_dispersion,All_Points_Not_Searched,gaze_x,gaze_y,radius)
+  
+        #La dispersion est trop importante, on vérifie que le rayon a changé et que ce n'est pas une microsaccade
+        if rayon_dispersion!=radius:
+            #On vérifie la présence d'une MicroSaccade, de plus on enlève de la liste des points les coordonnées de micro_saccade
+            Micro_Saccade,gaze_x,gaze_y,All_Points_Not_Searched,saccade = Check_Micro_Saccade_Salvucci(Micro_Saccade,start,end,gaze_x,gaze_y,saccade,time)
+            
+            if Micro_Saccade :
+                print("Micro")
+            #Il y a une fixation mais pas de micro saccade
+            else:
+                print("Dipserion : ",dispersion," start: ",start, "  end : ",end)
+                middle_fix = middle_calcul(gaze_x[start:end-1],gaze_y[start:end-1])
+                fixation.append((middle_fix[0],middle_fix[1],rayon_dispersion/3,time[start],time[end-1]-time[start]))  #The center of the fixation is saved
+                rayon_dispersion = radius #The dispersion is reset to its default value
+            
+            #On a besoin d'une saccade entre deux dispersion
+            while(No_Saccade and All_Points_Not_Searched):
+                #########Condition Temporel sur une saccade############
+                #une saccade ne doit pas être confondu avec un blink donc t > 20ms
+                while((time[end]-time[start])<0.02 and All_Points_Not_Searched):
+                    if end<(len(gaze_x)-2):
+                        end+=1
+                    else:
+                        All_Points_Not_Searched = False
+
+                if end<(len(gaze_x)-2):
+                    end+=1
+                else:
+                    All_Points_Not_Searched = False
+                while calcul_disp(gaze_x[start:end],gaze_y[start:end]) < rayon_dispersion and All_Points_Not_Searched:
+                    if end<(len(gaze_x)-2): end+=1
+                    else: All_Points_Not_Searched = False
+                No_Saccade = False
+                saccade.append(("Normal",gaze_x[start],gaze_y[start],gaze_x[end],gaze_y[end],time[start],time[end],time[end]-time[start]))
+                start = end  
+
+        #Si le rayon n'a pas changé alors on a une saccade (aucune fixation dans le laps de temps étudié)
+        else:
+            No_Saccade = False
+            saccade.append(("Normal",gaze_x[start],gaze_y[start],gaze_x[end],gaze_y[end],time[start],time[end],time[end]-time[start]))
+        start = end-1
+        dispersion = 0
+        
+        #On remet le flag Saccade à 1  
+        No_Saccade=True    
+        Micro_Saccade = False
+      
+    print("Cerlce Salvador : ",fixation)
+    return fixation,saccade
+
+def dispersion_map_Eucl(time,gaze_x,gaze_y,radius,duration):
+
+    """
+    Return a list of fixation : fixation (x,y,radius) if the conditions are respected
+    A circle is created, if the dispersion and time between the points are contained in their respective threshold.
+    Using method of Salvucci, D.D.; Goldberg, J.H. Identifying fixations and saccades in eye-tracking protocols.
+    The function first check if the distance between the point is longer than the radius and then if the time passed is sufficient.
+    """
+    rayon_dispersion = radius
+    duration_limit = duration
+    x_1 = gaze_x[0]
+    y_1 = gaze_y[0]
+    time_0 = time[0]
+    fixation= []
+    saccade=[]
+    micro_saccade=[]
+    middle_fix = []
+    #we search the distance between each point 
+    for i in range(1,len(gaze_x)-2):
+        if np.isnan(gaze_x[i])==False:
+            x_2 = gaze_x[i]
+            y_2 = gaze_y[i]
+            time_1 = time[i]
+            distance = mt.sqrt((x_1-x_2)**2 + (y_1-y_2)**2)
+            #Event if the fixation is over (out of the circle)
+            if distance > rayon_dispersion : 
+                #The dispersion size must have evolved to be considered a fixation
+                if rayon_dispersion != radius :
+                    #The time between 2 points must be high enough
+                    if time_1-time_0 >= duration_limit:
+                        start = np.where(time == time_0)[0][0]
+                        end = np.where(time == time_1)[0][0]
+                        print("Start : ",start)
+                        print("end : ",end)
+                        middle_fix = middle_calcul(gaze_x[start:end],gaze_y[start:end])
+                        fixation.append((middle_fix[0],middle_fix[1],rayon_dispersion,time_0,time_1-time_0))  #The center of the fixation is saved
+                        rayon_dispersion = radius #The dispersion is reset to its default value
+                x_1 = x_2
+                y_1 = y_2
+                time_0=time_1
+
+            elif rayon_dispersion < (radius*3):
+                rayon_dispersion = rayon_dispersion * 1.01
+    #Condition if the gaze stay in a circle and don't disperse at the end
+    if time_1-time_0 >= duration_limit:
+        start = np.where(time == time_0)[0][0]
+        end = np.where(time == time_1)[0][0]
+        print("Start : ",start)
+        print("end : ",end)
+        middle_fix = middle_calcul(gaze_x[start:end],gaze_y[start:end])
+        fixation.append((middle_fix[0],middle_fix[1],rayon_dispersion,time_0,time_1-time_0))  #The center of the fixation is saved
+        fixation.append((middle_fix[0],middle_fix[1],rayon_dispersion,time_0,time_1-time_0))  #The center of the fixation is saved
+    print("Cerlce Eucl : ",fixation)
+    return fixation,saccade
+
+def middle_calcul(x,y):
+    """
+    Fonction qui retourne les coordonées du centre d'une succession de points
+    """
+    sum_x = 0
+    sum_y = 0
+    for xp in x:
+        sum_x += xp
+    for yp in y:
+        sum_y += yp
+    return [sum_x/len(x),sum_y/len(y)]

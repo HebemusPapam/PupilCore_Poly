25322f2a4d02d1f138613f76267eb62b44a69ba5
clairorx <84668330+clairorx@users.noreply.github.com>
2023-03-27 14:51:01 +0200

    test IVT

--- Gaze_recording/Visualization/Methode_I_VT.py
+++ Gaze_recording/Visualization/Methode_I_VT.py
@@ -14,36 +14,40 @@ def fixation_velocity(threshold_speed, x, y, time): # Le tableau time est en sec
     threshold_time = 0.03 # Seuil de temps pour définir une saccade minimum (en secondes)
     threshold_speed = threshold_speed*1000 #Conversion du seuil en seconde :
     duration = 0 
+    start = 0
+    end = 0
+    faux_positif = 0
 
     tab_fixation = []
     tab_saccade = []
-    
-    temps_start = time 
-    temps_end = time 
-    speed = speed_calcul(x,y,time)
-    for i in range(len(x)-1):
-        while(time[i+1]-time[i]< threshold_time and  All_Points_Not_Searched==True):
-            duration = time[i+1]-time[i] #Temps de fixation
-            if speed[i] < threshold_speed :
-                tab_fixation.append((x[i],y[i],temps_start[i],temps_end[i+1],duration))
-            else : 
-                tab_saccade.append((x[i],y[i],temps_start[i],temps_end[i+1],duration))
-            i+=1 # Condition pour ne pas dépasser la taille du tableau
-            if i == len(x)-1:
-                All_Points_Not_Searched = False
-    return tab_fixation,tab_saccade
 
-# On modifie le tableau tab_fixation de sorte à ne pas avoir plusieurs points de fixation consécutifs 
-def tab_fixation_modification(tab_fixation):
-    """Fonction qui modifie le tableau tab_fixation de sorte à ne pas avoir plusieurs points de fixation consécutifs """
-    tab_fixation_modifie = []
-    for i in range(len(tab_fixation)):
-        if tab_fixation[i][0] == "Fixation" and tab_fixation[i+1][0] == "Fixation":
-            print("Fixation")
-            # On ne garde que le premier point = début de fixation, on supprime les lignes redondantes et on fait le calcul de la durée de fixation totale 
-        elif tab_fixation[i][0] == "Saccade" and tab_fixation[i+1][0] == "Saccade":
-            tab_fixation_modifie.append("Saccade")
-        else:
-            tab_fixation_modifie.append(tab_fixation[i])
-    return tab_fixation_modifie 
+
+    speed = speed_calcul(x,y,time)
+    while(All_Points_Not_Searched):
+        if end < len(x)-2 : 
+            end += 1
+        else: 
+            All_Points_Not_Searched = False
+        
+        if speed[end] < threshold_speed : 
+            duration += time[end] - time[start]
+ 
+        else : 
+            if faux_positif > 3:
+                if duration > 0.20 : 
+                    print("start : ", start)
+                    print("end : ", end-1)
+                    print("duration : ", duration)
+                start = end - 1
+                duration = 0
+                faux_positif = 0
+            else:
+                faux_positif += 1
+                duration += time[end] - time[start]
+                print("faux positif : ", faux_positif)
+                print("end : ", end)
+            
+    return tab_fixation, tab_saccade
+ 
+
  
